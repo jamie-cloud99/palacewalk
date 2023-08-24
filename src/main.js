de0f18217272250938dbacf93d27e0b8804dfe67
jamie-cloud99 <j1999j88@gmail.com>
2023-08-24 10:02:51 +0800

    build: 安裝 Vue3 Marquee、VCalendar 套件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { setupCalendar } from 'v-calendar'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(setupCalendar, {})
 
 app.mount('#app')
