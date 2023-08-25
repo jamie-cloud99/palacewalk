179b36208f2013edffbe8140f782145da16067bd
jamie-cloud99 <j1999j88@gmail.com>
2023-08-26 00:04:54 +0800

    fix: 刪除空class

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -3,7 +3,7 @@
     <div class="container flex justify-between py-6">
       <RouterLink to="/" class="flex gap-2 items-center">
         <img src="/images/logo.svg" alt="故宮走走" class="w-12 h-12 block" />
-        <div class="">
+        <div >
           <h1 class="font-serif font-bold text-[28px] leading-tight tracking-widest">故宮走走</h1>
           <h2 class="text-[14px] leading-none font-serif font-medium tracking-wider">
             Content Curator
@@ -46,7 +46,7 @@
           <i class="fa-solid fa-bars"></i>
         </button>
       </div>
-      <div v-if="isOpen.menu" class="">
+      <div v-if="isOpen.menu" >
         <button type="button" class="text-2xl lg:text-[28px] p-2" @click="toggleMenu('menu')">
           <i class="fa-solid fa-xmark"></i>
         </button>
