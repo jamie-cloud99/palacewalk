c270d19afd49e8d47fadc326bf3ffb6db55ab675
jamie-cloud99 <j1999j88@gmail.com>
2023-09-03 23:47:45 +0800

    fix: 修改回到頂端元件樣式

--- src/components/button/GoToTop.vue
+++ src/components/button/GoToTop.vue
@@ -1,8 +1,9 @@
 <template>
   <button
     ref="goToTopBtn"
-    class="fixed bottom-[237px] right-10 btn bg-[#444] z-[100] hover:bg-primary">
-    <i class="fa-solid fa-chevron-up text-white"></i>
+    class="fixed bottom-20 right-6 lg:right-10 btn bg-dark-200 z-[100] active:bg-dark hover:text-white lg:hover:bg-primary"
+  >
+    <i class="fa-solid fa-chevron-up"></i>
   </button>
 </template>
 
@@ -14,20 +15,21 @@ const scrollPosition = ref(0)
 
 const scrollFunction = () => {
   scrollPosition.value = window.scrollY
-  console.log('scrollPosition.value',scrollPosition.value)
-  scrollPosition.value > 100 ?
-  goToTopBtn.value.classList.remove("hidden") : goToTopBtn.value.classList.add("hidden")
-};
+  console.log('scrollPosition.value', scrollPosition.value)
+  scrollPosition.value > 100
+    ? goToTopBtn.value.classList.remove('lg:hidden')
+    : goToTopBtn.value.classList.add('lg:hidden')
+}
 const backToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" })
-};
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
 onMounted(() => {
-  window.addEventListener("scroll", scrollFunction)
-  goToTopBtn.value.addEventListener("click", backToTop)
+  window.addEventListener('scroll', scrollFunction)
+  goToTopBtn.value.addEventListener('click', backToTop)
 })
 
 onBeforeUnmount(() => {
-  window.removeEventListener("scroll", scrollFunction)
-  goToTopBtn.value.removeEventListener("click", backToTop)
+  window.removeEventListener('scroll', scrollFunction)
+  goToTopBtn.value.removeEventListener('click', backToTop)
 })
 </script>
