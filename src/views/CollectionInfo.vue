3ef0ba0d100afaba2caac8bff8ecb8264fa1127a
Han Wang <hanforwork896@gmail.com>
2023-09-04 23:41:04 +0800

    fix: fix conflicts

--- src/views/CollectionInfo.vue
+++ src/views/CollectionInfo.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>展品介紹</h1>
+  <CollectionBanner :title="'展品介紹'" />
+  <div class="container">
+    <div class="flex py-10 lg:py-15">
+      <PageComponent :pages="pages" @change="turnPage" />
+    </div>
+  </div>
+  <GoToTop />
 </template>
+
+<script setup>
+// import { reactive } from 'vue'
+import CollectionBanner from '../components/collection/CollectionBanner.vue'
+import GoToTop from '../components/button/GoToTop.vue'
+import PageComponent from '../components/layout/PageComponent.vue'
+
+</script>
+
