572347d89fb113e29c0b68717e7cd2d95dd5710d
jamie-cloud99 <j1999j88@gmail.com>
2023-09-11 17:05:38 +0800

    fix: 調整會員專區畫面 + 串接會員留言資料

--- src/views/MemberMessages.vue
+++ src/views/MemberMessages.vue
@@ -6,13 +6,13 @@
     </button>
   </div>
   <ul class="space-y-5">
-    <li v-for="i in 5" :key="i + 'i'">
+    <li v-for="comment in commentList" :key="comment.id">
       <div class="border-y border-dark-600 px-2">
         <div
           class="flex justify-between items-center gap-2 flex-wrap pt-4 pb-2 border-b border-dark"
         >
           <h3 class="text-warning-600 text-lg font-black line-clamp-1 lg:text-2xl">
-            風格故事—琺瑯彩瓷特展
+            {{ comment.exhibition.title }}
           </h3>
           <div class="hidden lg:flex justify-end flex-grow gap-2">
             <button
@@ -31,7 +31,7 @@
         </div>
         <div class="pt-4 pb-2">
           <p class="pb-2 lg:pb-4 min-h-[50px] font-medium">
-            之前去故宮看過一次這檔展覽，沒想到還有線上展可以重溫，琺瑯彩瓷的美歷經百年也不會褪色！
+            {{ comment.content }}
           </p>
           <div class="flex justify-end flex-grow gap-2 mb-3 lg:hidden">
             <button
@@ -52,28 +52,32 @@
               <button
                 type="button"
                 class="btn pl-0 text-dark-600 hover:text-dark"
-                @click="toggleReply(i)"
+                @click="toggleReply(comment.id)"
               >
-                <span v-if="isOpen === i">顯示</span><span v-else>隱藏</span>回覆
+                <span v-if="isOpen !== i">顯示</span><span v-else>隱藏</span>回覆
               </button>
               <a href="" class="btn pl-0 text-dark-600 hover:text-dark">查看原始留言</a>
             </div>
-            <p class="text-dark-600">2023/08/10</p>
+            <p class="text-dark-600">{{ comment.createdAt }}</p>
           </div>
           <div
             class="max-h-0 overflow-hidden transition-all duration-500"
-            :class="{ '!max-h-[500px]': isOpen === i }"
+            :class="{ '!max-h-[500px]': isOpen === comment.id }"
           >
             <hr class="border-dark-400 mb-3" />
             <div class="flex items-start gap-2">
-              <img src="https://fakeimg.pl/36" alt="" class="block flex-shrink-0 rounded-full" />
+              <img
+                :src="`images/user-${comment.reply.user.id}.webp`"
+                alt=""
+                class="block flex-shrink-0 rounded-full w-9 h-9"
+              />
               <div class="flex-grow">
                 <p class="text-warning-800 font-black leading-[36px]">策展人</p>
                 <div class="flex justify-between flex-wrap gap-2">
                   <p class="lg:mb-2">
-                    銅胎畫琺瑯方盤是康熙御製的傑作，看來你跟皇帝有同樣獨到的眼光呢！
+                    {{ comment.reply.content }}
                   </p>
-                  <p class="text-dark-600 flex-grow text-right">2023/08/10</p>
+                  <p class="text-dark-600 flex-grow text-right">{{ comment.reply.createdAt }}</p>
                 </div>
               </div>
             </div>
@@ -96,9 +100,54 @@ import PageComponent from '../components/layout/PageComponent.vue'
 const memberStore = useMemberStore()
 const { pages, turnPage } = memberStore
 
-const isOpen = ref('')
+const isOpen = ref('1')
 
 const toggleReply = (id) => {
   isOpen.value = isOpen.value === id ? '' : id
 }
+
+const commentList = ref([
+  {
+    id: '1',
+    content: '喜歡瓷胎粉彩玉堂富貴鼻煙壺，精緻又繽紛！',
+    createdAt: '2023.09.01',
+    exhibition: {
+      id: '1',
+      title: '士拿乎—清宮鼻煙壺的時尚風潮'
+    },
+    user: {
+      id: '1',
+      name: '文化探索者'
+    },
+    reply: {
+      user: {
+        id: '2',
+        name: '評論達人'
+      },
+      content: '我也很喜歡上面暈染細膩的花',
+      createdAt: '2023.09.02'
+    }
+  },
+  {
+    id: '2',
+    content: '展覽中都是故宮知名的展品，很適合想快速了解故宮文物的人欣賞',
+    createdAt: '2023.09.13',
+    exhibition: {
+      id: '3',
+      title: '故宮經典—藝術與文化策展'
+    },
+    user: {
+      id: '1',
+      name: '文化探索者'
+    },
+    reply: {
+      user: {
+        id: '2',
+        name: '評論達人'
+      },
+      content: '謝謝你喜歡，歡迎多來逛逛！',
+      createdAt: '2023.09.16'
+    }
+  }
+])
 </script>
