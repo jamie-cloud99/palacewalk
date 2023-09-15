70418e9f49f8567c3c0f46b454c455c79ef1d10e
jamie-cloud99 <j1999j88@gmail.com>
2023-09-15 17:07:01 +0800

    fix: resolve merge conflicts

--- src/components/exhibition/ExhibitionListItem.vue
+++ src/components/exhibition/ExhibitionListItem.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="group relative text-white overflow-hidden">
-<<<<<<< HEAD
     <button
       type="button"
       class="absolute top-0 right-0 p-2 text-xl z-[1] hover:text-primary"
@@ -12,28 +11,15 @@
       :src="`/images/exhibitions/exhibition-${exhibitionItem.exhibitionId}.jpg`"
       class="w-full h-[250px] xl:h-[320px] object-cover transition-all ease-in-out duration-300 group-hover:scale-110"
     />
-=======
-    <button type="button" class="absolute top-0 right-0 p-2 text-xl z-[1] group-hover:text-primary"
-      @click="saveFavorites(exhibitionItem, 'exhibition')">
-      <i class="fa-regular fa-heart" :class="{ 'fa-solid': showFavorite }"> </i>
-    </button>
-    <img :src="`/images/exhibitions/exhibition-${exhibitionItem.exhibitionId}.jpg`"
-      class="w-full object-cover transition-all ease-in-out duration-300 group-hover:scale-110" />
->>>>>>> 5ae4881bf0fcb4034cb4f50fad687f286b990e4e
     <div class="w-full bg-black/50 text-white px-4 py-2 absolute bottom-0 left-0">
       <h3 class="font-semibold text-xl line-clamp-1 mb-1">{{ exhibitionItem.title }}</h3>
       <p class="font-medium">{{ exhibitionItem.startDate }} - {{ exhibitionItem.endDate }}</p>
     </div>
 
-<<<<<<< HEAD
     <RouterLink
       class="stretched-link"
       :to="{ name: 'exhibitionIntro', params: { exhibitionId: exhibitionItem.exhibitionId } }"
     />
-=======
-    <RouterLink class="stretched-link"
-      :to="{ name: 'exhibitionIntro', params: { exhibitionId: exhibitionItem.exhibitionId } }" />
->>>>>>> 5ae4881bf0fcb4034cb4f50fad687f286b990e4e
   </div>
 </template>
 
