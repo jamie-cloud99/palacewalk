0bb1879418d81a0f2d20fd22feaaab901c49f284
jamie-cloud99 <j1999j88@gmail.com>
2023-09-15 21:54:05 +0800

    fix: 修正圖片路徑

--- src/components/exhibition/ExhibitionBanner.vue
+++ src/components/exhibition/ExhibitionBanner.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="h-[260px] bg-no-repeat bg-[url('./images/exhibitions/exhibition-banner.png'),_url('./images/exhibitions/page-banner1-1.jpg')] bg-[position:82%_18px,_center_bottom] xs:bg-[size:200px,_cover] lg:bg-[size:244px,_cover]">
+		class="h-[260px] bg-no-repeat bg-[url('/images/exhibitions/exhibition-banner.png'),_url('/images/exhibitions/page-banner1-1.jpg')] bg-[position:82%_18px,_center_bottom] xs:bg-[size:200px,_cover] lg:bg-[size:244px,_cover]">
 		<div class="container relative text-white">
 			<h2 class="absolute top-[115px] text-3xl font-serif font-bold px-2 drop-shadow-md">
 				{{ bannerContent.title }}
