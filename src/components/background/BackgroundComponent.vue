d4e915b929551a1df6df0b6c45571bf703e3165c
Han Wang <hanforwork896@gmail.com>
2023-09-15 23:29:34 +0800

    fix: code formatted

--- src/components/background/BackgroundComponent.vue
+++ src/components/background/BackgroundComponent.vue
@@ -1,17 +1,18 @@
 <template>
   <div
     class="
-    absolute -z-10 
-    top-[600px] 
-    bg-[url('../images/page-bg.svg')] 
-    bg-[position:center] 
-    object-cover 
-    bg-no-repeat h-[1000px] 
-    md:h-[760px] 
-    w-full 
-    xl:bg-center 
-    xl:h-[800px] 
-    min-[1920px]:h-[1000px] 
+    absolute -z-10
+    top-[600px]
+    bg-[url('../images/page-bg.svg')]
+    bg-[position:center]
+    object-cover
+    bg-no-repeat
+    h-[1000px]
+    md:h-[760px]
+    w-full
+    xl:bg-center
+    xl:h-[800px]
+    min-[1920px]:h-[1000px]
     min-[1920px]:bg-cover"
   ></div>
 </template>
