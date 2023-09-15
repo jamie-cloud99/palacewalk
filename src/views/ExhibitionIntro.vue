efda6ba486a3a81882b6af485e9fc6672c3a817d
jamie-cloud99 <j1999j88@gmail.com>
2023-09-15 16:55:07 +0800

    fix: 修正元件資料傳遞錯誤

--- src/views/ExhibitionIntro.vue
+++ src/views/ExhibitionIntro.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="mb-6 lg:mb-[68px]">
-		<ExhibitionBanner :banner-content="curBannerContent" />
-	</div>
+    <ExhibitionBanner :banner-content="curBannerContent" />
+  </div>
   <div class="bg-[url('/images/page-bg.svg')] bg-[position:0_216px,_left_top] bg-no-repeat">
     <div class="container mb-[60px]">
       <div class="lg:grid grid-cols-12 gap-x-12">
         <div class="col-span-3">
-          <ExhibitionSort 
-						:menu="menuContent" 
-						:selectedOption="curMenuItem" 
-						@select-item="changeMenuItem" 
-					/>
+          <h3 class="hidden lg:flex flex-col text-lg font-semibold">
+            展覽
+            <span class="font-cormo text-2xl">EXHIBITS</span>
+          </h3>
+          <hr class="border-b border-dark-400 lg:border-dark" />
+          <div class="mb-6 lg:mb-20">
+            <SortList
+              :menu="menuContent"
+              :selectedOption="curMenuItem"
+              @select-item="changeMenuItem"
+            ></SortList>
+          </div>
+          <div class="hidden lg:block">
+            <h3 class="flex flex-col text-lg">
+              檢索
+              <span class="font-cormo text-2xl">SEARCH</span>
+            </h3>
+            <ul class="mb-4">
+              <li class="mb-3 relative">
+                <input type="text" id="search" class="form-input search" placeholder="展覽檢索" />
+                <button
+                  type="button"
+                  class="btn absolute top-1/2 right-0 -translate-y-1/2 hover:text-primary"
+                >
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </li>
+              <li class="relative">
+                <select name="" id="search" class="form-select search">
+                  <option class="text-dark-600" value="" disabled selected>請選擇類別</option>
+                  <option class="text-dark" value="">藝術</option>
+                  <option class="text-dark" value="">文物</option>
+                  <option class="text-dark" value="">綜合</option>
+                </select>
+              </li>
+            </ul>
+          </div>
         </div>
         <div class="col-span-9 font-semibold">
-          <img class="w-full h-[340px] object-cover object-center mb-2 lg:mb-4" src='/images/exhibitions/exhibition-U001.jpg'>
+          <img
+            class="w-full h-[340px] object-cover object-center mb-2 lg:mb-4"
+            src="/images/exhibitions/exhibition-U001.jpg"
+          />
           <div class="flex flex-col justify-between mb-8 lg:flex-row">
             <div class="mb-4 lg:mb-0">
               <h2 class="text-2xl font-bold mb-4">{{ exhibitionTitle }}</h2>
@@ -22,15 +57,24 @@
             <div class="flex flex-row-reverse justify-between lg:flex-col lg:justify-start">
               <div class="flex mb-2">
                 <button type="button" class="btn border border-dark-800 px-5 mr-4">回列表</button>
-                <button type="button" 
+                <button
+                  type="button"
                   class="btn bg-primary text-white"
                   @click="router.push('/exhibitions/1/content')"
-                >前往看展</button>
+                >
+                  前往看展
+                </button>
               </div>
               <ul class="flex justify-end text-2xl">
-                <li class="me-4"><a href="#"><i class="fa-regular fa-calendar"></i></a></li>
-                <li class="me-4"><a href="#"><i class="fa-regular fa-heart"></i></a></li>
-                <li><a href="#"><i class="fa-solid fa-share-nodes"></i></a></li>
+                <li class="me-4">
+                  <a href="#"><i class="fa-regular fa-calendar"></i></a>
+                </li>
+                <li class="me-4">
+                  <a href="#"><i class="fa-regular fa-heart"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-solid fa-share-nodes"></i></a>
+                </li>
               </ul>
             </div>
           </div>
@@ -40,8 +84,8 @@
             <h3 class="font-bold mb-2">展覽亮點：</h3>
             <div v-html="content" class="content mb-8"></div>
             <h3 class="font-bold mb-2">展品資訊：</h3>
-            <div  class="overflow-hidden mb-8">
-							<CollectionSlides />
+            <div class="overflow-hidden mb-8">
+              <CollectionSlides />
             </div>
             <h4 class="font-bold mb-2">展覽留言<span class="comment-num">(2)</span></h4>
             <div class="relative border-b pb-[24px] mb-3">
@@ -61,9 +105,13 @@
                   <div class="pt-4 pb-2">
                     <div class="flex items-center">
                       <div class="w-[36px] h-[36px] mr-2">
-                        <img src="/images/user-3.webp" class="object-cover object-center" alt="文化探索者">
+                        <img
+                          src="/images/user-3.webp"
+                          class="object-cover object-center"
+                          alt="文化探索者"
+                        />
                       </div>
-                      <div class=" mr-2">文化探索者</div>
+                      <div class="mr-2">文化探索者</div>
                       <time class="text-primary">2023/08/10</time>
                     </div>
                   </div>
@@ -71,8 +119,7 @@
                     <p class="pb-2 pl-11 lg:pb-4 min-h-[50px] font-medium">
                       之前去故宮看過一次這檔展覽，沒想到還有線上展可以重溫，琺瑯彩瓷的美歷經百年也不會褪色！
                     </p>
-                    <div class="flex justify-end flex-grow gap-2 mb-3 lg:hidden">
-                    </div>
+                    <div class="flex justify-end flex-grow gap-2 mb-3 lg:hidden"></div>
                     <div class="flex justify-between items-center">
                       <div class="flex gap-2 pl-11">
                         <button
@@ -92,7 +139,11 @@
                       <div class="pl-6 gap-2">
                         <div class="flex items-center">
                           <div class="w-[36px] h-[36px] mr-2">
-                            <img src="/images/user-2.webp" class="object-cover object-center" alt="策展者">
+                            <img
+                              src="/images/user-2.webp"
+                              class="object-cover object-center"
+                              alt="策展者"
+                            />
                           </div>
                           <p class="text-warning-800 font-black leading-[36px]">策展人</p>
                           <time class="text-primary">2023/08/10</time>
@@ -115,10 +166,10 @@
   </div>
 </template>
 <script setup>
-import { ref, reactive,computed } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import ExhibitionBanner from '../components/exhibition/ExhibitionBanner.vue'
-import ExhibitionSort from '../components/exhibition/ExhibitionSort.vue'
+import SortList from '../components/layout/SortList.vue'
 import CollectionSlides from '../components/exhibition/CollectionSlides.vue'
 
 import { storeToRefs } from 'pinia'
@@ -128,43 +179,42 @@ const route = useRoute()
 const router = useRouter()
 
 const breadList = reactive([
-	{
-		title: '首頁',
-		path: '/'
-	},
-	{
-		title: '展覽空間',
-		path: '/exhibitions'
-	},
-	{
-		title: '當期展覽',
-		path: '/exhibitions'
-	},
-	{
-		title: '展覽簡介',
-		path: '/exhibitionIntro'
-	}
+  {
+    title: '首頁',
+    path: '/'
+  },
+  {
+    title: '展覽空間',
+    path: '/exhibitions'
+  },
+  {
+    title: '當期展覽',
+    path: '/exhibitions'
+  },
+  {
+    title: '展覽簡介',
+    path: '/exhibitionIntro'
+  }
 ])
 const menuContent = reactive([
-	{
-		code: 'recent',
-		title: '當期展覽'
-	},
-	{
-		code: 'coming',
-		title: '近期展覽'
-	}
+  {
+    code: 'recent',
+    title: '當期展覽'
+  },
+  {
+    code: 'coming',
+    title: '近期展覽'
+  }
 ])
 const curMenuItem = ref(menuContent[0])
 const curBannerContent = computed(() => {
-	return { title: `展覽空間 — ${curMenuItem.value.title}`, breadList }
+  return { title: `展覽空間 — ${breadList[breadList.length - 1].title}`, breadList }
 })
 
-
 const changeMenuItem = (item) => {
-	curMenuItem.value = item
-	breadList[breadList.length - 1].title = item.title
-	router.push({ path: `${route.path}`, query: { period: item.title } })
+  curMenuItem.value = item
+  breadList[breadList.length - 1].title = item.title
+  router.push({ path: `/exhibitions`, query: { period: item.title } })
 }
 
 const exhibitsStore = exhibitionStore()
@@ -203,12 +253,12 @@ const content = computed({
 })
 </script>
 <style>
-  .content ol{
-    list-style-type: decimal;
-    padding-left: 24px;
-    line-height: 1.5;
-  }
-  .content li{
-    margin-bottom: 8px;
-  }
+.content ol {
+  list-style-type: decimal;
+  padding-left: 24px;
+  line-height: 1.5;
+}
+.content li {
+  margin-bottom: 8px;
+}
 </style>
