525ebbf97d8f72b3004f723fd696842d1f09d8ab
jamie-cloud99 <j1999j88@gmail.com>
2023-09-15 13:31:26 +0800

    fix: 調整展品介紹頁

---
+++ src/components/collection/CollectionMasterSlides.vue
@@ -0,0 +1,96 @@
+<template>
+  <swiper-container
+    class="lg:overflow-hidden"
+    :space-between="24"
+    :scrollbar="true"
+    :slides-per-view="2"
+    :breakpoints="{ 1024: { slidesPerView: 4 } }"
+  >
+    <swiper-slide>
+      <figure class="w-full h-[210px] lg:h-[215px]">
+        <img
+          class="w-full h-full object-cover object-centers"
+          :src="`/images/collection/collection-P001.jpg`"
+          alt="P001"
+        />
+      </figure>
+    </swiper-slide>
+    <swiper-slide>
+      <figure class="w-full h-[210px] lg:h-[215px]">
+        <img
+          class="w-full h-full object-cover object-center"
+          :src="`/images/collection/collection-P002.jpg`"
+          alt="P002"
+        />
+      </figure>
+    </swiper-slide>
+    <swiper-slide>
+      <figure class="w-full h-[210px] lg:h-[215px]">
+        <img
+          class="w-full h-full object-cover object-center"
+          :src="`/images/collection/collection-P003.jpg`"
+          alt="P003"
+        />
+      </figure>
+    </swiper-slide>
+    <swiper-slide>
+      <figure class="w-full h-[210px] lg:h-[215px]">
+        <img
+          class="w-full h-full object-cover object-center"
+          :src="`/images/collection/collection-P003.jpg`"
+          alt="P003"
+        />
+      </figure>
+    </swiper-slide>
+  </swiper-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// import CollectionListItem from './CollectionListItem.vue'
+
+const MasterPeiceList  = ref([
+    {
+      id: '401',
+      collectionId: 'K004',
+      title: '緙繡九羊啟泰 軸',
+      author: '不詳',
+      time: '清'
+    },
+    {
+      id: '14',
+      collectionId: 'P014',
+      title: '畫春花三種 軸',
+      author: '錢維城',
+      time: '清'
+    },
+    {
+      id: '1',
+      collectionId: 'U001',
+      title: '翠玉白菜',
+      author: '不詳',
+      time: '明末清初'
+    },
+    {
+      id: '15',
+      collectionId: 'P015',
+      title: '奔馬行空　單片',
+      author: '徐悲鴻',
+      time: '民國'
+    },
+    {
+      id: '16',
+      collectionId: 'P016',
+      title: '翠巘高秋圖　軸',
+      author: '愛新覺羅弘旿',
+      time: '清'
+    },
+    {
+      id: '201',
+      collectionId: 'A005',
+      title: '琺瑯彩柳燕碗',
+      author: '不詳',
+      time: '清'
+    }])
+
+</script>
--- src/views/CollectionInfo.vue
+++ src/views/CollectionInfo.vue
@@ -1,30 +1,32 @@
 <template>
   <!-- TODO: recommend swiper-->
   <CollectionBanner :title="'展品介紹'" />
-  <div class="absolute left-0 right-0 top-[570px] h-[800px] w-full -z-10 bg-[url('../images/page-bg.svg')] bg-cover bg-center bg-no-repeat"></div>
+  <div
+    class="absolute left-0 right-0 top-[570px] h-[800px] w-full -z-10 bg-[url('../images/page-bg.svg')] bg-cover bg-center bg-no-repeat"></div>
   <div class="container">
     <swiper-container
-      class="mb-10 pxs-0 lg:px-[15px] overflow-visible lg:overflow-hidden"
-      :space-between="24"
+      class="mb-10 px-0 lg:px-4 overflow-visible lg:overflow-hidden"
+      :space-between="16"
       :slides-per-view="2"
       :breakpoints="{ 1024: { slidesPerView: 2 } }"
-      ref="swiperEl"
       navigation="true"
-      >
-
+    >
       <swiper-slide class="w-[300px]">
-        <figure class="w-full h-[210px] lg:h-[360px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P001.jpg`' alt="P001">
+        <figure class="w-full h-[210px] lg:h-[360px] bg-gradient-to-b from-dark/80 to-dark/90">
+          <img
+            class="w-full h-full object-contain object-center"
+            :src="`/images/collection/collection-P024.jpg`"
+            alt="元朱叔重秋山疊翠 軸"
+          />
         </figure>
       </swiper-slide>
-      <swiper-slide>
+      <swiper-slide v-for="i in 3" :key="i + 'i'" class="w-[300px]">
         <figure class="w-full h-[210px] lg:h-[360px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P002.jpg`' alt="P002">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="w-full h-[210px] lg:h-[360px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P003.jpg`' alt="P003">
+          <img
+            class="w-full h-full object-cover object-center"
+            :src="`/images/collection/collection-P024-${i}.jpg`"
+            alt="元朱叔重秋山疊翠 軸"
+          />
         </figure>
       </swiper-slide>
     </swiper-container>
@@ -41,13 +43,27 @@
       </div>
     </div>
     <div class="font-semibold mb-11">
-      <p class="font-bold mb-2">朱叔重（約西元十四世紀）也有人寫作叔仲，江蘇吳縣人。他雖然是個讀書人，但在繪畫上也有卓越的成就，可惜遺留下來的作品很少。 小橋對岸，有林木蕭疏，村屋四五家隱約可見。山嶺連綿蜿蜒而上，白雲深鎖於山際。雲山蒼潤，水墨淋漓。山作直皴，樹用橫筆米點，畫法似出自米芾、高克恭。</p>
-      <p class="mb-[42px]">Chu Shu-ch'ung, a native of Kiang-su, was a Confucian scholar, who excelled in painting. Whenever he wrote a particularly successful poem, he would paint a picture capturing the poetic concept. This painting was completed in1365. As an official Chu Shu-ch'ung had little time to paint, thus few of his paintings are still extant. In this scroll, the trees are painted with horizontal dabs of ink, recalling the style of Mi Fu and Mi Yu-Jen. The mountains are constructed with vertical brushstrokes and heavy ink, resembling the work of Wu Chen.</p>
+      <p class="font-bold mb-2">
+        朱叔重（約西元十四世紀）也有人寫作叔仲，江蘇吳縣人。他雖然是個讀書人，但在繪畫上也有卓越的成就，可惜遺留下來的作品很少。
+        小橋對岸，有林木蕭疏，村屋四五家隱約可見。山嶺連綿蜿蜒而上，白雲深鎖於山際。雲山蒼潤，水墨淋漓。山作直皴，樹用橫筆米點，畫法似出自米芾、高克恭。
+      </p>
+      <p class="mb-[42px]">
+        Chu Shu-ch'ung, a native of Kiang-su, was a Confucian scholar, who excelled in painting.
+        Whenever he wrote a particularly successful poem, he would paint a picture capturing the
+        poetic concept. This painting was completed in1365. As an official Chu Shu-ch'ung had little
+        time to paint, thus few of his paintings are still extant. In this scroll, the trees are
+        painted with horizontal dabs of ink, recalling the style of Mi Fu and Mi Yu-Jen. The
+        mountains are constructed with vertical brushstrokes and heavy ink, resembling the work of
+        Wu Chen.
+      </p>
       <h3 class="text-2xl font-bold mb-4">藝品賞析</h3>
-      <p class="mb-[42px]">《元朱叔重秋山疊翠》是元代著名畫家朱叔重的傑作之一，以其獨特的墨色表現手法吸引人們。畫中山川起伏，勾勒煙雲繚繞的秋景，墨色處處流轉，疊翠叠翠，賦予作品生命力與變化。輕重運用墨，勾勒山勢、岩石、樹木，細緻入微，使山水層次分明，景深立體。簡潔的筆法，透過墨色的變化，描繪出自然景物的氣勢和情感。作品中的山水與情感相互交融，令人感受到作者對大自然的敬仰與感嘆之情。整幅畫營造出一種寧靜與宏偉並存的意境，讓觀者彷彿置身於其中，感受自然的美妙與壯麗。這幅作品展現了朱叔重對山水畫的深刻理解和造詣，成為中國山水畫的經典之作。</p>
+      <p class="mb-[42px]">
+        《元朱叔重秋山疊翠》是元代著名畫家朱叔重的傑作之一，以其獨特的墨色表現手法吸引人們。畫中山川起伏，勾勒煙雲繚繞的秋景，墨色處處流轉，疊翠叠翠，賦予作品生命力與變化。輕重運用墨，勾勒山勢、岩石、樹木，細緻入微，使山水層次分明，景深立體。簡潔的筆法，透過墨色的變化，描繪出自然景物的氣勢和情感。作品中的山水與情感相互交融，令人感受到作者對大自然的敬仰與感嘆之情。整幅畫營造出一種寧靜與宏偉並存的意境，讓觀者彷彿置身於其中，感受自然的美妙與壯麗。這幅作品展現了朱叔重對山水畫的深刻理解和造詣，成為中國山水畫的經典之作。
+      </p>
       <h3 class="text-2xl font-bold mb-4">作者簡介</h3>
-      <p class="mb-[42px]">朱叔重（1271年-1336年），元代著名畫家，字道襄，號澄園，生於浙江寧波。他是元代南宗畫派的代表人物之一，以山水畫見長，並且在畫風上有獨特的貢獻。朱叔重崇尚宋代文人畫風，注重表達內心情感，追求淡雅、虛實結合的境界。<br>
-        他的畫作融合了北宋文人畫的文雅與南宋宗派畫的技巧，精於運用淡墨疏密，以抒發情感，將山水融入人情，將心境融入山水。他的作品《秋山疊翠》、《洪都圖》等，都以其獨特的筆法和情感表達成為後世傳世之寶。<br>
+      <p class="mb-[42px]">
+        朱叔重（1271年-1336年），元代著名畫家，字道襄，號澄園，生於浙江寧波。他是元代南宗畫派的代表人物之一，以山水畫見長，並且在畫風上有獨特的貢獻。朱叔重崇尚宋代文人畫風，注重表達內心情感，追求淡雅、虛實結合的境界。<br />
+        他的畫作融合了北宋文人畫的文雅與南宋宗派畫的技巧，精於運用淡墨疏密，以抒發情感，將山水融入人情，將心境融入山水。他的作品《秋山疊翠》、《洪都圖》等，都以其獨特的筆法和情感表達成為後世傳世之寶。<br />
         朱叔重對後世影響深遠，不僅在藝術上對山水畫風有重要影響，更在文化思想上提倡以內心修養為主的藝術創作理念。他的藝術作品充滿詩情畫意，為中國山水畫的發展做出了卓越的貢獻。
       </p>
       <h3 class="text-2xl font-bold mb-4">詳細資訊</h3>
@@ -77,43 +93,20 @@
           <span>69.5x26.4 (公分)</span>
         </div>
         <!-- <div class="border-5 border-black border-b border-dashed py-[26px] text-center"> -->
-          <!-- <span class="mr-6">現收藏地</span>
+        <!-- <span class="mr-6">現收藏地</span>
           <span>故宮博物院</span> -->
         <!-- </div> -->
       </div>
-      <h3 class="text-2xl font-bold mb-4">精選展品推薦</h3>
-      <swiper-container
-      class="mb-10 lg:overflow-hidden"
-      :space-between="24"
-      :scrollbar="true"
-      :slides-per-view="2"
-      :breakpoints="{ 1024: { slidesPerView: 4 } }"
-      ref="swiperEl"
-    >
-      <swiper-slide>
-        <figure class="w-full h-[210px] lg:h-[215px]">
-          <img class="w-full h-full object-cover object-centers" :src='`/images/collection/collection-P001.jpg`' alt="P001">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="w-full h-[210px] lg:h-[215px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P002.jpg`' alt="P002">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="w-full h-[210px] lg:h-[215px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P003.jpg`' alt="P003">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="w-full h-[210px] lg:h-[215px]">
-          <img class="w-full h-full object-cover object-center" :src='`/images/collection/collection-P003.jpg`' alt="P003">
-        </figure>
-      </swiper-slide>
-    </swiper-container>
+      <h3 class="text-2xl font-bold mb-4">精選推薦</h3>
+      <div class="mb-10">
+        <CollectionMasterSlides />
+      </div>
     </div>
     <router-link :to="previousPage">
-      <button @click="previousPage" class="btn hover:bg-primary hover:text-white font-semibold border border-black mx-auto">
+      <button
+        @click="previousPage"
+        class="btn hover:bg-primary hover:text-white font-semibold border border-black mx-auto"
+      >
         回前一頁
       </button>
     </router-link>
@@ -124,13 +117,15 @@
 </template>
 
 <script setup>
-import { ref, reactive, watch } from 'vue'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import CollectionBanner from '../components/collection/CollectionBanner.vue'
 import PageComponent from '../components/layout/PageComponent.vue'
-import { useRouter } from 'vue-router'
+import CollectionMasterSlides from '../components/collection/CollectionMasterSlides.vue'
+
+
 const router = useRouter()
 
-const swiperEl = ref(null)
 const detail = reactive([
   {
     data: [
@@ -140,27 +135,14 @@ const detail = reactive([
         title: '秋山疊翠',
         author: '朱叔重',
         time: '元順帝至正二十五年(1365)',
-        ingredient : '紙',
+        ingredient: '紙',
         type: '山水',
-        size: '69.5x26.4 (公分)',
-      },
+        size: '69.5x26.4 (公分)'
+      }
     ]
-  },
-])
-const slides = ref({
-  totalSlides: 3,
-  curSlide: 1,
-  haveNext: true,
-  havePrev: false
-})
-
-watch(
-  () => slides.value.curSlide,
-  () => {
-    slides.value.havePrev = slides.value.curSlide <= 1 ? false : true
-    slides.value.haveNext = slides.value.curSlide >= slides.value.totalSlides - 1 ? false : true
   }
-)
+])
+
 
 const previousPage = () => {
   router.back()
@@ -170,6 +152,7 @@ const previousPage = () => {
 <style scoped>
 swiper-container::part(button-next),
 swiper-container::part(button-prev) {
+  position: absolute;
   color: black;
   width: 20px;
   height: 20px;
@@ -178,12 +161,10 @@ swiper-container::part(button-prev) {
 }
 
 swiper-container::part(button-next) {
-  position: absolute;
   right: -20px;
 }
 
 swiper-container::part(button-prev) {
-  position: absolute;
   left: -20px;
 }
 </style>
