5ec4a1e6db62f8e8f17d361a5f8cc50ee5a2d4a7
Han Wang <hanforwork896@gmail.com>
2023-10-07 10:52:37 +0800

    feat: 測試loading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <HeaderComponent v-if="!route.meta.isFullPage" />
+  <div>
+    <loading-component
+    :active="isLoading"
+    z-index="2000"
+    :color="'#97C897'"
+    />
+  </div>
   <RouterView />
   <GoToTop />
   <FooterComponent v-if="!route.meta.isFullPage" />
@@ -7,6 +14,7 @@
 
 <script setup>
 import { storeToRefs } from 'pinia'
+import { ref, onMounted } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
 import HeaderComponent from './components/layout/HeaderComponent.vue'
 import FooterComponent from './components/layout/FooterComponent.vue'
@@ -22,4 +30,9 @@ const { isLoggedIn } = storeToRefs(memberStore)
   await checkLogin()
   if (isLoggedIn.value) fetchFavorites()
 })()
+
+const isLoading = ref(false)
+onMounted(() => {
+  isLoading.value = true
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
 
 import { register } from 'swiper/element/bundle'
 register()
@@ -19,5 +21,9 @@ app.use(router)
 app.use(setupCalendar, {})
 app.use(VueAxios, axios)
 app.provide('axios', app.config.globalProperties.axios)
+app.use(Loading.Plugin);
 
+// Global Component
+app.component('loading-component', Loading)
+console.log('Loading',Loading)
 app.mount('#app')
