0607a9c79e81f2ba2ff2730972bbbfc109f1969e
jamie-cloud99 <j1999j88@gmail.com>
2023-10-07 12:14:07 +0800

    fix: 調整 axios 設定

--- src/stores/memberStore.js
+++ src/stores/memberStore.js
@@ -23,122 +23,6 @@ export const useMemberStore = defineStore('member', () => {
     imageUrl:
       'https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
   })
-  // const favoriteList = ref({
-  //   exhibitions: [
-  //     {
-  //       savedAt: '2023.09.01',
-  //       exhibition: {
-  //         id: '2',
-  //         exhibitionId: 'U002',
-  //         title: '風格故事—琺瑯彩瓷特展 ',
-  //         startDate: '2023.7.7',
-  //         endDate: '2024.7.16'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.08.28',
-  //       exhibition: {
-  //         id: '3',
-  //         exhibitionId: 'U003',
-  //         title: '故宮經典-藝術與文化策展',
-  //         startDate: '2023.6.20',
-  //         endDate: '2024.3.28'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.08.21',
-  //       exhibition: {
-  //         id: '4',
-  //         exhibitionId: 'U004',
-  //         title: '繽彩燒窯—開光雙連瓶藝術展',
-  //         startDate: '2023.6.24',
-  //         endDate: '2024.5.28'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.08.29',
-  //       exhibition: {
-  //         id: '5',
-  //         exhibitionId: 'U005',
-  //         title: '釉瓷之美：太平有象瓷尊特展',
-  //         startDate: '2023.5.20',
-  //         endDate: '2024.8.19'
-  //       }
-  //     }
-  //   ],
-  //   collections: [
-  //     {
-  //       savedAt: '2023.09.10',
-  //       collection: {
-  //         id: '15',
-  //         collectionId: 'P015',
-  //         title: '奔馬行空　單片',
-  //         author: '徐悲鴻',
-  //         time: '民國'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.09.10',
-  //       collection: {
-  //         id: '13',
-  //         collectionId: 'P013',
-  //         title: '方壺圖 軸',
-  //         author: '文伯仁',
-  //         time: '明'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.09.04',
-  //       collection: {
-  //         id: '16',
-  //         collectionId: 'P016',
-  //         title: '翠巘高秋圖　軸',
-  //         author: '愛新覺羅弘旿',
-  //         time: '清'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.09.04',
-  //       collection: {
-  //         id: '22',
-  //         collectionId: 'P022',
-  //         title: '摹顧愷之洛神圖 卷',
-  //         author: '丁觀鵬',
-  //         time: '清'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.09.04',
-  //       collection: {
-  //         id: '23',
-  //         collectionId: 'P023',
-  //         title: '紅牡丹　單片',
-  //         author: '鄭曼青',
-  //         time: '民國'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.09.04',
-  //       collection: {
-  //         id: '401',
-  //         collectionId: 'K004',
-  //         title: '緙繡九羊啟泰 軸',
-  //         author: '不詳',
-  //         time: '清'
-  //       }
-  //     },
-  //     {
-  //       savedAt: '2023.08.29',
-  //       collection: {
-  //         id: '206',
-  //         collectionId: 'A006',
-  //         title: '金胎內填兼畫琺瑯西方仕女圖執壺',
-  //         author: '不詳',
-  //         time: '清'
-  //       }
-  //     }
-  //   ],
-  // })
 
   const favoriteList = ref({
     exhibitions: [],
@@ -186,12 +70,21 @@ export const useMemberStore = defineStore('member', () => {
 
     if (isFavorite(itemId)) {
       // !fix  api - delete (刪除一筆會把全部資料都刪掉)
-      // const favId = favExhibitList.find((el) => el.exhibition.id === id)?.favId
-      // const apiUrl = ${VITE_JSON_SERVER}600/favorites/${favId}
-      // console.log(apiUrl)
+      let favId
+      if (type === 'exhibitions') {
+        favId = favExhibitions.value.find((el) => el.id === itemId)?.favId
+      } else {
+        favId = favCollections.value.find((el) => el.id === itemId)?.favId
+      }
+
+      const apiUrl = `${VITE_JSON_SERVER}600/favorites/${favId}`
+      console.log(apiUrl)
       // const res = await axios.delete(apiUrl)
-      // console.log(res)
-      // fetchFavorites();
+      const res = await axios.delete(apiUrl, (req) => {
+        req.options._noRemoveDependents = true
+      })
+      console.log(res)
+      fetchFavorites()
     } else {
       const nowTimeStamp = Math.floor(new Date().getTime() / 1000)
       const apiUrl = `${VITE_JSON_SERVER}600/${type}/${itemId}/favorites`
