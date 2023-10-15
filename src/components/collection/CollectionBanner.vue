32eb75e156c0b8bad3f1859703be5e7b30220dc9
Han Wang <hanforwork896@gmail.com>
2023-10-15 13:07:27 +0800

    feat: remove collectionMaster banner

--- src/components/collection/CollectionBanner.vue
+++ src/components/collection/CollectionBanner.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 目前沒被使用，但仍予保留，未來可能需用到-->
   <div class="h-[260px] mb-[68px] lg:bg-[size:350px,_cover] bg-[size:0px,_cover] bg-no-repeat
     bg-[url('/images/collection/collection-banner.png'),_url('/images/collection/banner-bg.jpg')]
     bg-[position:82%_-50%,_center_bottom]">
